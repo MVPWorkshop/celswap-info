10a78ab5675d917420a430b22c43f02d43fac9f9
andrejrakic <andrej.rakic@mvpworkshop.co>
2020-12-16 01:11:50 +0100

    Updates

---
+++ precache-manifest.5d3bdb7c7cf80a576968c068d50833fc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ad9b981a0e78fcb31797d8f95211877",
+    "url": "/celswap-info/index.html"
+  },
+  {
+    "revision": "82e30b4488bb6432f406",
+    "url": "/celswap-info/static/js/2.fab8c4a1.chunk.js"
+  },
+  {
+    "revision": "4392ce25eff6dc466cce05721ae9c54e",
+    "url": "/celswap-info/static/js/2.fab8c4a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "432f711a01b256370590",
+    "url": "/celswap-info/static/js/main.e0029bc0.chunk.js"
+  },
+  {
+    "revision": "dc0d31b38ea5560f60e2",
+    "url": "/celswap-info/static/js/runtime-main.7edd2aad.js"
+  },
+  {
+    "revision": "624a65b64c8667547f1cd087e0b05587",
+    "url": "/celswap-info/static/media/cel-swap-logo-symbol.624a65b6.svg"
+  },
+  {
+    "revision": "775521e0324c73ceed9e317b4212d3fc",
+    "url": "/celswap-info/static/media/celswap_logo.775521e0.svg"
+  },
+  {
+    "revision": "73dabb371ddce7c640334fe0904dcddf",
+    "url": "/celswap-info/static/media/eth.73dabb37.png"
+  }
+]);
